fd6848d8acd6ecf208a58650352401c22d61573a
Jellopys <61058386+Jellopys@users.noreply.github.com>
2024-12-14 18:13:42 +0100

    Character Stats

---
+++ CharacterStats.cpp
@@ -0,0 +1,151 @@
+﻿#include "CharacterStats.h"
+
+#include "CharacterStatType.h"
+#include "SecondaryStats.h"
+#include "SchoolTesting/Player/PlayerCharacter.h"
+#include "SchoolTesting/Player/CustomComponents/HealthComponent.h"
+#include "SchoolTesting/Player/CustomComponents/Magic/ManaComponent.h"
+
+void FCharacterStats::InitializeStats(APlayerCharacter* Player)
+{
+	MeleeAttack = DefaultMeleeAttack + (Strength) + (Luck * 0.5f) + UpgradedMeleeAtk;
+	MagicAttack = DefaultMagicAttack + (Intelligence) + (Luck * 0.5f) + UpgradedMagicAtk;
+	RangedAttack = DefaultRangedAttack + (Dexterity) + (Luck * 0.5f) + UpgradedRangedAtk;
+	AtkSpeed = DefaultAtkSpeed + (Agility * 0.005f) + (UpgradedAtkSpeed * 0.005f);
+	Hit = DefaultHit + (Dexterity * 0.5f) + (Luck * 0.5f) + UpgradedHit;
+	Armor = DefaultArmor + (Vitality * 0.5f) + (Agility * 0.5f) + UpgradedArmor;
+	Critical = DefaultCritical + (Luck * 0.5f) + UpgradedCrit;
+	
+	ExpToLevel = FMath::Pow(CurrentLevel * ExpReqGrowth, ExpReqGrowthExponential);
+
+	if (Player)
+	{
+		Player->ManaComponent->IncreaseIntelligence(Intelligence, CurrentLevel);
+		Player->HealthComponent->IncreaseVitality(Vitality, CurrentLevel);
+		Player->HealthComponent->CurrentHealth = Player->HealthComponent->MaxHealth;
+		Player->ManaComponent->CurrentMana = Player->ManaComponent->MaxMana;
+	}
+}
+
+void FCharacterStats::ResetStats()
+{
+	Strength -= PointsInStr;
+	Agility -= PointsInAgi; 
+	Intelligence -= PointsInInt;
+	Dexterity -= PointsInDex;
+	Vitality -= PointsInVit;
+	Luck -= PointsInLuk;
+
+	PointsInStr = 0;
+	PointsInAgi = 0;
+	PointsInInt = 0;
+	PointsInDex = 0;
+	PointsInVit = 0;
+	PointsInLuk = 0;
+	AvailableStatPoints = StatPointGainPerLevel * CurrentLevel;
+
+	MeleeAttack = DefaultMeleeAttack + (Strength) + (Luck * 0.5f) + UpgradedMeleeAtk;
+	MagicAttack = DefaultMagicAttack + (Intelligence) + (Luck * 0.5f) + UpgradedMagicAtk;
+	RangedAttack = DefaultRangedAttack + (Dexterity) + (Luck * 0.5f) + UpgradedRangedAtk;
+	AtkSpeed = DefaultAtkSpeed + (Agility * 0.005f) + (UpgradedAtkSpeed * 0.005f);
+	Hit = DefaultHit + (Dexterity * 0.5f) + (Luck * 0.5f) + UpgradedHit;
+	Armor = DefaultArmor + (Vitality * 0.5f) + (Agility * 0.5f) + UpgradedArmor;
+	Critical = DefaultCritical + (Luck * 0.5f) + UpgradedCrit;
+}
+
+void FCharacterStats::UpgradeStat(APlayerCharacter* Player, int UpgradeAmount, ECharacterStatType StatType, bool FromEquipment)
+{
+	switch (StatType)
+	{
+	case ECharacterStatType::Strength :
+		Strength += UpgradeAmount;
+		MeleeAttack = DefaultMeleeAttack + (Strength) + (Luck * 0.5f) + UpgradedMeleeAtk;
+		if (!FromEquipment)
+			PointsInStr++;
+		break;
+	case ECharacterStatType::Agility :
+		Agility += UpgradeAmount;
+		AtkSpeed = DefaultAtkSpeed + (Agility * 0.005f) + (UpgradedAtkSpeed * 0.005f);
+		Armor = DefaultArmor + (Vitality * 0.5f) + (Agility * 0.5f) + UpgradedArmor;
+		if (!FromEquipment)
+			PointsInAgi++;
+		break;
+	case ECharacterStatType::Intelligence :
+		Intelligence += UpgradeAmount;
+		Player->ManaComponent->IncreaseIntelligence(Intelligence, CurrentLevel);
+		MagicAttack = DefaultMagicAttack + (Intelligence) + (Luck * 0.5f) + UpgradedMagicAtk;
+		if (!FromEquipment)
+			PointsInInt++;
+		break;
+	case ECharacterStatType::Dexterity :
+		Dexterity += UpgradeAmount;
+		Hit = DefaultHit + (Dexterity * 0.5f) + (Luck * 0.5f) + UpgradedHit;
+		RangedAttack = DefaultRangedAttack + (Dexterity) + (Luck * 0.5f) + UpgradedRangedAtk;
+		if (!FromEquipment)
+			PointsInDex++;
+		break;
+	case ECharacterStatType::Vitality :
+		Vitality += UpgradeAmount;
+		Player->HealthComponent->IncreaseVitality(Vitality, CurrentLevel);
+		Armor = DefaultArmor + (Vitality * 0.5f) + (Agility * 0.5f) + UpgradedArmor;
+		if (!FromEquipment)
+			PointsInVit++;
+		break;
+	case ECharacterStatType::Luck :
+		Luck += UpgradeAmount;
+		Hit = DefaultHit + (Dexterity * 0.5f) + (Luck * 0.5f) + UpgradedHit;
+		MeleeAttack = DefaultMeleeAttack + (Strength) + (Luck * 0.5f) + UpgradedMeleeAtk;
+		MagicAttack = DefaultMagicAttack + (Intelligence) + (Luck * 0.5f) + UpgradedMagicAtk;
+		RangedAttack = DefaultRangedAttack + (Dexterity) + (Luck * 0.5f) + UpgradedRangedAtk;
+		Critical = DefaultCritical + (Luck * 0.5f) + UpgradedCrit;
+		if (!FromEquipment)
+			PointsInLuk++;
+		break;
+	default:
+		break;
+	}
+
+	if (!FromEquipment)
+	{
+		AvailableStatPoints = FMath::Clamp(AvailableStatPoints - 1, 0, 999);
+	}
+}
+
+
+void FCharacterStats::UpgradeSecondaryStat(APlayerCharacter* Player, int UpgradeAmount, ESecondaryStats SecondaryStat)
+{
+	switch (SecondaryStat)
+	{
+	case ESecondaryStats::Armor :
+		UpgradedArmor += UpgradeAmount;
+		Armor = DefaultArmor + (Vitality * 0.5f) + (Agility * 0.5f) + UpgradedArmor;
+		break;
+	case ESecondaryStats::Critical :
+		UpgradedCrit += UpgradeAmount;
+		Critical = DefaultCritical + (Luck * 0.5f) + UpgradedCrit;
+		break;
+	case ESecondaryStats::Hit :
+		UpgradedHit += UpgradeAmount;
+		Hit = DefaultHit + (Dexterity * 0.5f) + (Luck * 0.5f) + UpgradedHit;
+		break;
+	case ESecondaryStats::AtkSpeed :
+		UpgradedAtkSpeed += UpgradeAmount;
+		AtkSpeed = DefaultAtkSpeed + (Agility * 0.005f) + (UpgradedAtkSpeed * 0.005f);
+		break;
+	case ESecondaryStats::MagicAttack :
+		UpgradedMagicAtk += UpgradeAmount;
+		MagicAttack = DefaultMagicAttack + (Intelligence) + (Luck * 0.5f) + UpgradedMagicAtk;
+		break;
+	case ESecondaryStats::MeleeAttack :
+		UpgradedMeleeAtk += UpgradeAmount;
+		MeleeAttack = DefaultMeleeAttack + (Strength) + (Luck * 0.5f) + UpgradedMeleeAtk;
+		break;
+	case ESecondaryStats::RangedAttack :
+		UpgradedRangedAtk += UpgradeAmount;
+		RangedAttack = DefaultRangedAttack + (Dexterity) + (Luck * 0.5f) + UpgradedRangedAtk;
+		break;
+		
+		default:
+			break;
+	}
+}
---
+++ CharacterStats.h
@@ -0,0 +1,110 @@
+﻿// Richard Hill
+
+#pragma once
+#include "SecondaryStats.h"
+
+#include "CharacterStats.generated.h"
+
+class APlayerCharacter;
+
+enum class ECharacterStatType : uint8;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnLevelUp, int, StatPointGain);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGainExp, int, ExperienceGained);
+USTRUCT(BlueprintType)
+struct FCharacterStats
+{
+	GENERATED_BODY();
+	
+	// Base stats
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BaseStats)
+	double Strength = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BaseStats)
+	double Agility = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BaseStats)
+	double Intelligence = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BaseStats)
+	double Dexterity = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BaseStats)
+	double Vitality = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = BaseStats)
+	double Luck = 1;
+
+	UPROPERTY(BlueprintReadWrite)
+	int PointsInStr = 0;
+	UPROPERTY(BlueprintReadWrite)
+	int PointsInAgi = 0;
+	UPROPERTY(BlueprintReadWrite)
+	int PointsInInt = 0;
+	UPROPERTY(BlueprintReadWrite)
+	int PointsInDex = 0;
+	UPROPERTY(BlueprintReadWrite)
+	int PointsInVit = 0;
+	UPROPERTY(BlueprintReadWrite)
+	int PointsInLuk = 0;
+	
+	int UpgradedMeleeAtk = 0;
+	int UpgradedRangedAtk = 0;
+	int UpgradedMagicAtk = 0;
+	int UpgradedHit = 0;
+	int UpgradedArmor = 0;
+	int UpgradedCrit = 0;
+	int UpgradedAtkSpeed = 0;
+
+	// Default stats
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = DefaultStats)
+	double DefaultMeleeAttack = 10;
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = DefaultStats)
+	double DefaultRangedAttack = 10;
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = DefaultStats)
+	double DefaultMagicAttack = 10;
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = DefaultStats)
+	double DefaultHit = 10;
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = DefaultStats)
+	double DefaultArmor = 10;
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = DefaultStats)
+	double DefaultCritical = 10;
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = DefaultStats)
+	double DefaultAtkSpeed = 1;
+
+	// Stats
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	double MeleeAttack = 10;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	double RangedAttack = 10;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	double MagicAttack = 10;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	double Hit = 10;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	double Armor = 10;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	double Critical = 10;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	double AtkSpeed = 100;
+
+	UPROPERTY(BlueprintReadWrite, Category = Stats)
+	int AvailableStatPoints = 4;
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Stats)
+	int StatPointGainPerLevel = 4;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Experience)
+	int CurrentLevel = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Experience)
+	float CurrentExp = 0;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Experience)
+	float ExpToLevel = 100;
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Experience)
+	float ExpReqGrowth = 10;
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Experience)
+	float ExpReqGrowthExponential = 2;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Experience)
+	float ExpWorth = 1;
+
+	void InitializeStats(APlayerCharacter* Player);
+	void ResetStats();
+	void UpgradeStat(APlayerCharacter* Player, int UpgradeAmount, ECharacterStatType StatType, bool FromEquipment);
+	void UpgradeSecondaryStat(APlayerCharacter* Player, int UpgradeAmount, ESecondaryStats SecondaryStat);
+	FOnLevelUp OnLevelUp;
+	FOnGainExp OnGainExp;
+};
